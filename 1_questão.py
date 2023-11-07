bc1514f842d6c478f3d722a0639e06194516a743
Raiane-Cipriano <cicero.oliveira139@aluno.ce.gov.br>
2023-11-07 10:34:10 -0300

    6/27
    lista 02

--- 1_questão.py
+++ 1_questão.py
@@ -2,5 +2,5 @@ n1 = float(input("Digite o primeiro número: "))
 n2 = float(input("Digite o segundo número: "))
 if n1>n2:
     print("O número maior é: ", n1)
-else:
+elif:
     print("O número maior é: ", n2)
