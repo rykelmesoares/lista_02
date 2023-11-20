b2ab098a832838c5545e5cdcde987e95a139bb38
rykelmesoares <cicero.oliveira139@aluno.ce.gov.br>
2023-11-20 08:35:57 -0300

    10/27
    lista 02

---
+++ 10_questão.py
@@ -0,0 +1,9 @@
+turno=input("Digite seu turno, M, V, N: ")
+if turno =="M":
+    print("Bom dia!")
+elif turno =="V":
+    print("Boa tarde!")
+elif turno =="N":
+    print("Boa noite!")
+else:
+    print("Valor inválido!")
